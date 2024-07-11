2b22fe9fa6f3ffc5d0a3e0bd47b831ec86b09178
Álefe Cruz <98561479+Raissatkm@users.noreply.github.com>
2024-07-10 21:21:14 -0300

    Create estudo.java

---
+++ estudo.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class TipoDeDados {
+  public static void main(String[] args) {
+      int numeroInteiro = 10;
+      double numeroDecimal = 10.5;
+      char caractere = 'A'; 
+      String texto = "Olá, mundo!";
+      boolean verdadeiroFalso = true;
+     
+      System.out.println(numeroInteiro);
+      System.out.println(numeroDecimal);
+      System.out.println(caractere);
+      System.out.println(texto);
+      System.out.println(verdadeiroFalso);
+  }
+}
